a2a0fb74108c550b6c76649ac07974b0104208eb
netarchy <netarchy@gmail.com>
2011-04-21 21:52:18 -0500

    Correct some voltage settings and add the ability to modify voltages via sysfs based on work by TheEscapistxda
    Signed-off-by: nubecoder <nubecoder@gmail.com>

--- Kernel/arch/arm/mach-s5pv210/cpu-freq.c
+++ Kernel/arch/arm/mach-s5pv210/cpu-freq.c
@@ -50,6 +50,9 @@ static unsigned int mpll_freq; /* in MHz */
 static unsigned int apll_freq_max; /* in MHz */
 static DEFINE_MUTEX(set_freq_lock);
 
+/* UV */
+extern int exp_UV_mV[8];
+
 /* frequency */
 static struct cpufreq_frequency_table freq_table[] = {
 	{L0, 1400*1000},
@@ -81,11 +84,11 @@ const unsigned long int_volt_max = 1250000;
 static struct s5pv210_dvs_conf dvs_conf[] = {
 	[L0] = {
 		.arm_volt   = 1450000,
-		.int_volt   = 1150000,
+		.int_volt   = 1250000,
 	},
 	[L1] = {
 		.arm_volt   = 1400000,
-		.int_volt   = 1150000,
+		.int_volt   = 1200000,
 	},
 	[L2] = {
 		.arm_volt   = 1350000,
@@ -536,8 +539,11 @@ static int s5pv210_cpufreq_target(struct cpufreq_policy *policy,
 	if (s3c_freqs.freqs.new == s3c_freqs.freqs.old && !first_run)
 		goto out;
 
-	arm_volt = dvs_conf[index].arm_volt;
+	arm_volt = (dvs_conf[index].arm_volt + (exp_UV_mV[index] * 1000));
+//	arm_volt = dvs_conf[index].arm_volt;
 	int_volt = dvs_conf[index].int_volt;
+
+	printk("setting vdd %d for speed %d\n", arm_volt, arm_clk);
 
 	/* New clock information update */
 	memcpy(&s3c_freqs.new, &clk_info[index],
@@ -723,7 +729,8 @@ static int s5pv210_cpufreq_target(struct cpufreq_policy *policy,
 	memcpy(&s3c_freqs.old, &s3c_freqs.new, sizeof(struct s3c_freq));
 	cpufreq_debug_printk(CPUFREQ_DEBUG_DRIVER, KERN_INFO,
 			"cpufreq: Performance changed[L%d]\n", index);
-	previous_arm_volt = dvs_conf[index].arm_volt;
+//	previous_arm_volt = dvs_conf[index].arm_volt;
+	previous_arm_volt = (dvs_conf[index].arm_volt + (exp_UV_mV[index] * 1000));
 
 	if (first_run)
 		first_run = false;
@@ -766,7 +773,8 @@ static int s5pv210_cpufreq_resume(struct cpufreq_policy *policy)
 
 	memcpy(&s3c_freqs.old, &clk_info[level],
 			sizeof(struct s3c_freq));
-	previous_arm_volt = dvs_conf[level].arm_volt;
+//	previous_arm_volt = dvs_conf[level].arm_volt;
+	previous_arm_volt = (dvs_conf[level].arm_volt + (exp_UV_mV[level] * 1000));
 
 	return ret;
 }
@@ -834,7 +842,8 @@ static int __init s5pv210_cpufreq_driver_init(struct cpufreq_policy *policy)
 
 	memcpy(&s3c_freqs.old, &clk_info[level],
 			sizeof(struct s3c_freq));
-	previous_arm_volt = dvs_conf[level].arm_volt;
+//	previous_arm_volt = dvs_conf[level].arm_volt;
+	previous_arm_volt = (dvs_conf[level].arm_volt + (exp_UV_mV[level] * 1000));
 
 #ifdef CONFIG_DVFS_LIMIT
         for(i = 0; i < DVFS_LOCK_TOKEN_NUM; i++)
--- Kernel/arch/arm/mach-s5pv210/mach-victory.c
+++ Kernel/arch/arm/mach-s5pv210/mach-victory.c
@@ -707,7 +707,7 @@ static struct regulator_init_data victory_buck2_data = {
 		.valid_ops_mask	= REGULATOR_CHANGE_VOLTAGE |
 				  REGULATOR_CHANGE_STATUS,
 		.state_mem	= {
-			.uV	= 1150000,
+			.uV	= 1250000,
 			.mode	= REGULATOR_MODE_NORMAL,
 			.disabled = 1,
 		},

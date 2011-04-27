f56c4eda4e39942143bfc04d6b71af6c4f08f1d5
netarchy <netarchy@gmail.com>
2011-04-26 21:49:33 -0500

    Modify sysfs vdd interface so that the user can set vdd directly instead of the old approach of setting a modifier that was then added or subtracted before being fed into the regulator. The old approach presented occasional problems.
    Signed-off-by: nubecoder <nubecoder@gmail.com>

--- Kernel/arch/arm/mach-s5pv210/cpu-freq.c
+++ Kernel/arch/arm/mach-s5pv210/cpu-freq.c
@@ -539,8 +539,8 @@ static int s5pv210_cpufreq_target(struct cpufreq_policy *policy,
 	if (s3c_freqs.freqs.new == s3c_freqs.freqs.old && !first_run)
 		goto out;
 
-	arm_volt = (dvs_conf[index].arm_volt + (exp_UV_mV[index] * 1000));
-//	arm_volt = dvs_conf[index].arm_volt;
+//	arm_volt = (dvs_conf[index].arm_volt + (exp_UV_mV[index] * 1000));
+	arm_volt = exp_UV_mV[index]; //dvs_conf[index].arm_volt;
 	int_volt = dvs_conf[index].int_volt;
 
 //	printk("setting vdd %d for speed %d\n", arm_volt, arm_clk);
@@ -730,7 +730,8 @@ static int s5pv210_cpufreq_target(struct cpufreq_policy *policy,
 	cpufreq_debug_printk(CPUFREQ_DEBUG_DRIVER, KERN_INFO,
 			"cpufreq: Performance changed[L%d]\n", index);
 //	previous_arm_volt = dvs_conf[index].arm_volt;
-	previous_arm_volt = (dvs_conf[index].arm_volt + (exp_UV_mV[index] * 1000));
+	previous_arm_volt = exp_UV_mV[index];
+//	previous_arm_volt = (dvs_conf[index].arm_volt + (exp_UV_mV[index] * 1000));
 
 	if (first_run)
 		first_run = false;
@@ -773,8 +774,8 @@ static int s5pv210_cpufreq_resume(struct cpufreq_policy *policy)
 
 	memcpy(&s3c_freqs.old, &clk_info[level],
 			sizeof(struct s3c_freq));
-//	previous_arm_volt = dvs_conf[level].arm_volt;
-	previous_arm_volt = (dvs_conf[level].arm_volt + (exp_UV_mV[level] * 1000));
+	previous_arm_volt = exp_UV_mV[level]; //dvs_conf[level].arm_volt;
+//	previous_arm_volt = (dvs_conf[level].arm_volt + (exp_UV_mV[level] * 1000));
 
 	return ret;
 }
@@ -842,8 +843,8 @@ static int __init s5pv210_cpufreq_driver_init(struct cpufreq_policy *policy)
 
 	memcpy(&s3c_freqs.old, &clk_info[level],
 			sizeof(struct s3c_freq));
-//	previous_arm_volt = dvs_conf[level].arm_volt;
-	previous_arm_volt = (dvs_conf[level].arm_volt + (exp_UV_mV[level] * 1000));
+	previous_arm_volt = exp_UV_mV[level]; //dvs_conf[level].arm_volt;
+//	previous_arm_volt = (dvs_conf[level].arm_volt + (exp_UV_mV[level] * 1000));
 
 #ifdef CONFIG_DVFS_LIMIT
         for(i = 0; i < DVFS_LOCK_TOKEN_NUM; i++)

47cb73fe977bebf014df52fb1436e896965e24db
nubecoder <nubecoder@gmail.com>
2011-11-05 23:26:40 -0400

    Reworking the move /system/app code (less by default, more configurable).
    Signed-off-by: nubecoder <nubecoder@gmail.com>

--- initramfs_eh17/nubernel/scripts/init_01_system_apps.sh
+++ initramfs_eh17/nubernel/scripts/init_01_system_apps.sh
@@ -7,6 +7,10 @@
 # http://www.nubecoder.com/
 #
 
+#defines
+MV_SYS_APPS_FILE="/data/local/.mv_sys_apps"
+RM_SYS_APPS_FILE="/data/local/.rm_sys_apps"
+
 #functions
 SEND_LOG()
 {
@@ -14,6 +18,9 @@ SEND_LOG()
 }
 REMOVE_SYSTEM_APP()
 {
+	local ARGS="$1"
+	set -- junk $ARGS;
+	shift
 	local APK="$1"
 	local DATA="$2"
 	if /sbin/busybox test -f "/system/app/$APK.apk"; then
@@ -29,34 +36,43 @@ REMOVE_SYSTEM_APP()
 		/sbin/busybox rm -rf /data/data/$DATA
 	fi
 }
-
 MOVE_SYSTEM_APP()
 {
+	local ARGS="$1"
+	set -- junk $ARGS;
+	shift
 	local APK="$1"
 	local DATA="$2"
-	local CACHE="$3"
-	if /sbin/busybox test -f "/system/app/$APK.odex"; then
-		SEND_LOG "  Detected odex file, app will not be moved"
-		return 1
-	fi
+	local CACHE="system@app@${APK}.apk@classes.dex"
+
+	SEND_LOG "APK[$APK] DATA[$DATA] CACHE[$CACHE]"
+
 	if /sbin/busybox test -f "/system/app/$APK.apk"; then
-		SEND_LOG "  Detected apk file, moving app to /data/app"
+		SEND_LOG "  Detected apk file, moving to /data/app"
 		local EXISTING_APP_FIND=$(/sbin/busybox find /data/app -iname "${DATA}*")
 		for EXISTING_APP in $EXISTING_APP_FIND ; do
 			SEND_LOG "  rm -f $EXISTING_APP"
 			/sbin/busybox rm -f "$EXISTING_APP"
 		done
 		SEND_LOG "  mv -f /system/app/$APK.apk /data/app/${DATA}-1.apk"
-		/sbin/busybox mv -f /system/app/$APK.apk /data/app/${DATA}-1.apk
-		/sbin/busybox chown system.system /data/app/${DATA}-1.apk
-		/sbin/busybox chmod 0644 /data/app/${DATA}-1.apk
+		/sbin/busybox mv -f "/system/app/$APK.apk" "/data/app/${DATA}-1.apk"
+		/sbin/busybox chown system.system "/data/app/${DATA}-1.apk"
+		/sbin/busybox chmod 0644 "/data/app/${DATA}-1.apk"
+
+		if /sbin/busybox test -f "/system/app/$APK.odex"; then
+			SEND_LOG "  Detected odex file, moving to /data/app"
+			SEND_LOG "  mv -f /system/app/$APK.odex /data/app/${DATA}-1.odex"
+			/sbin/busybox mv -f "/system/app/$APK.odex" "/data/app/${DATA}-1.odex"
+			/sbin/busybox chown system.system "/data/app/${DATA}-1.odex"
+			/sbin/busybox chmod 0644 "/data/app/${DATA}-1.odex"
+		fi
 		if /sbin/busybox test -d "/data/data/$DATA"; then
 			SEND_LOG "  rm -rf /data/data/$DATA"
-			/sbin/busybox rm -rf /data/data/$DATA
+			/sbin/busybox rm -rf "/data/data/$DATA"
 		fi
 		if /sbin/busybox test -f "/data/dalvik-cache/$CACHE"; then
 			SEND_LOG "  rm -f /data/dalvik-cache/$CACHE"
-			/sbin/busybox rm -f /data/dalvik-cache/$CACHE
+			/sbin/busybox rm -f "/data/dalvik-cache/$CACHE"
 		fi
 	fi
 }
@@ -69,24 +85,41 @@ if /sbin/busybox test "$1" = "recovery"; then
 else
 	SEND_LOG "Ensuring there is room for busybox and root"
 
-	SEND_LOG "  Moving market downloadable apps to /data/app"
-	MOVE_SYSTEM_APP "amazonmp3" "com.amazon.mp3" "system@app@amazonmp3.apk@classes.dex"
-	MOVE_SYSTEM_APP "BooksPhone" "com.google.android.apps.books" "system@app@BooksPhone.apk@classes.dex"
-	MOVE_SYSTEM_APP "FBAndroid-1.5.4" "com.facebook.katana" "system@app@FBAndroid-1.5.4.apk@classes.dex"
-	MOVE_SYSTEM_APP "Gmail" "com.google.android.gm" "system@app@Gmail.apk@classes.dex"
-	MOVE_SYSTEM_APP "install_flash_player" "com.adobe.flashplayer" "system@app@install_flash_player.apk@classes.dex"
-	MOVE_SYSTEM_APP "Maps" "com.google.android.apps.maps" "system@app@Maps.apk@classes.dex"
-	MOVE_SYSTEM_APP "MediaHubV126" "com.sdgtl.mediahub.spr" "system@app@MediaHubV126.apk@classes.dex"
-	MOVE_SYSTEM_APP "qik-8.66-release-ffc" "com.qikffc.android" "system@app@qik-8.66-release-ffc.apk@classes.dex"
-	MOVE_SYSTEM_APP "Street" "com.google.android.street" "system@app@Street.apk@classes.dex"
-	MOVE_SYSTEM_APP "YouTube" "com.google.android.youtube" "system@app@YouTube.apk@classes.dex"
+	SEND_LOG "Looking for $MV_SYS_APPS_FILE"
+	if /sbin/busybox test -f $MV_SYS_APPS_FILE; then
+		SEND_LOG "  Using: $MV_SYS_APPS_FILE"
+	else
+		echo "Asphalt5_DEMO_SAMSUNG_D700_Sprint_ML_330 com.gameloft.android.ANMP.GloftAsphalt5Demo.asphalt5" >$MV_SYS_APPS_FILE
+		echo "FreeHDGameDemos com.gameloft.microwidget" >>$MV_SYS_APPS_FILE
+		echo "nascar09-prod-release com.handson.h2o.nascar09" >>$MV_SYS_APPS_FILE
+		echo "qik-8.66-release-ffc com.qikffc.android" >>$MV_SYS_APPS_FILE
+		echo "sfl-prod-release com.handson.h2o.nfl" >>$MV_SYS_APPS_FILE
+		echo "SprintTV com.mobitv.client.sprinttv" >>$MV_SYS_APPS_FILE
+		echo "SprintTVWidget com.mobitv.sprint.tvwidget" >>$MV_SYS_APPS_FILE
+		echo "SprintZone com.sprint.dsa" >>$MV_SYS_APPS_FILE
+		echo "TN6.2-sprint-handset-6201501 com.telenav.app.android.sprint" >>$MV_SYS_APPS_FILE
+		SEND_LOG "  Created: $MV_SYS_APPS_FILE"
+	fi
+	SEND_LOG "  Moving apps to /data/app"
+	while read LINE ; do
+		MOVE_SYSTEM_APP "$LINE"
+	done < $MV_SYS_APPS_FILE
 
-	SEND_LOG "  Removing 5 copys of Term.apk (why Samsung, why?)"
-	REMOVE_SYSTEM_APP "Term1" "com.android.term1"
-	REMOVE_SYSTEM_APP "Term2" "com.android.term2"
-	REMOVE_SYSTEM_APP "Term3" "com.android.term3"
-	REMOVE_SYSTEM_APP "Term4" "com.android.term4"
-	REMOVE_SYSTEM_APP "Term5" "com.android.term5"
+	SEND_LOG "Looking for $RM_SYS_APPS_FILE"
+	if /sbin/busybox test -f $RM_SYS_APPS_FILE; then
+		SEND_LOG "  Using: $RM_SYS_APPS_FILE"
+	else
+		echo "Term1 com.android.term1" >$RM_SYS_APPS_FILE
+		echo "Term2 com.android.term2" >>$RM_SYS_APPS_FILE
+		echo "Term3 com.android.term3" >>$RM_SYS_APPS_FILE
+		echo "Term4 com.android.term4" >>$RM_SYS_APPS_FILE
+		echo "Term5 com.android.term5" >>$RM_SYS_APPS_FILE
+		SEND_LOG "  Created: $RM_SYS_APPS_FILE"
+	fi
+	SEND_LOG "  Removing apps from /system/app"
+	while read LINE ; do
+		REMOVE_SYSTEM_APP "$LINE"
+	done < $RM_SYS_APPS_FILE
 
 	SEND_LOG "Sync filesystem"
 	/system/xbin/busybox sync

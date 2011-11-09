b00380ac6ce216a81d7d112cc0c8e37508f5c69c
nubecoder <nubecoder@gmail.com>
2011-11-09 18:59:18 -0500

    Fixing some errors I introduced in the cpufreq sysfs entries.
    Signed-off-by: nubecoder <nubecoder@gmail.com>

--- Kernel/drivers/cpufreq/cpufreq.c
+++ Kernel/drivers/cpufreq/cpufreq.c
@@ -33,6 +33,7 @@
 						"cpufreq-core", msg)
 
 /* UV */
+extern unsigned int freq_uv_table[11][3];
 int exp_UV_mV[11] =     { 1450000, 1400000, 1350000, 1300000, 1275000, 1225000, 1200000, 1175000, 1050000, 950000, 950000 };
 int enabled_freqs[11] = {       0,       0,       0,       1,       1,       1,       1,        1,      1,      1,      1 };
 /*  clock freqs         {  1.4GHz,  1.3GHz,  1.2GHz,  1.1GHz,  1.0GHz,  900MHz,  800MHz,  600MHz,  400MHz, 200MHz, 100MHz } */
@@ -655,18 +656,18 @@ static ssize_t show_scaling_setspeed(struct cpufreq_policy *policy, char *buf)
 /* sysfs interface for UV control */
 static ssize_t show_UV_mV_table(struct cpufreq_policy *policy, char *buf) {
 
-	return sprintf(buf, "1400mhz: %d mV\n1300mhz: %d mV\n1200mhz: %d mV\n1100mhz: %d mV\n\
-1000mhz: %d mV\n900mhz: %d mV\n800mhz: %d mV\n600mhz: %d mV\n400mhz: %d mV\n200mhz: %d mV\n100mhz: %d mV\n",
-			exp_UV_mV[0]/1000, exp_UV_mV[1]/1000, exp_UV_mV[2]/1000, exp_UV_mV[3]/1000,
-			exp_UV_mV[4]/1000, exp_UV_mV[5]/1000, exp_UV_mV[6]/1000, exp_UV_mV[7]/1000,
-			exp_UV_mV[8]/1000, exp_UV_mV[9]/1000), exp_UV_mV[10]/1000);
-
+	return sprintf(buf, "1400mhz: %d mV\n1300mhz: %d mV\n1200mhz: %d mV\n1100mhz: %d mV\n1000mhz: %d mV\n\
+900mhz: %d mV\n800mhz: %d mV\n600mhz: %d mV\n400mhz: %d mV\n200mhz: %d mV\n100mhz: %d mV\n",
+			(exp_UV_mV[0]/1000), (exp_UV_mV[1]/1000), (exp_UV_mV[2]/1000), (exp_UV_mV[3]/1000), (exp_UV_mV[4]/1000),
+			(exp_UV_mV[5]/1000), (exp_UV_mV[6]/1000), (exp_UV_mV[7]/1000), (exp_UV_mV[8]/1000), (exp_UV_mV[9]/1000),
+			(exp_UV_mV[10]/1000));
 }
 
 static ssize_t store_UV_mV_table(struct cpufreq_policy *policy,
 					const char *buf, size_t count) {
 
 	unsigned int ret = -EINVAL;
+	int i = 0;
 	ret = sscanf(buf, "%d %d %d %d %d %d %d %d %d %d %d",
 			&exp_UV_mV[0], &exp_UV_mV[1], &exp_UV_mV[2], &exp_UV_mV[3],
 			&exp_UV_mV[4], &exp_UV_mV[5], &exp_UV_mV[6], &exp_UV_mV[7],
@@ -674,19 +675,20 @@ static ssize_t store_UV_mV_table(struct cpufreq_policy *policy,
 	if(ret != 11) {
 		return -EINVAL;
 	}
-	else
-		for( i = 0; i < 11; i++ )
+	else {
+		for(i = 0; i < 11; i++)
 		{
 		   exp_UV_mV[i] *= 1000;
 		}
+	}
 		return count;
 }
 
 static ssize_t show_frequency_voltage_table(struct cpufreq_policy *policy,
 						char *buf) {
 
-	return sprintf(buf,
-			"%d %d %d\n%d %d %d\n%d %d %d\n%d %d %d\n%d %d %d\n%d %d %d\n%d %d %d\n%d %d %d\n%d %d %d\n",
+	return sprintf(buf, "%d %d %d\n%d %d %d\n%d %d %d\n%d %d %d\n%d %d %d\n\
+%d %d %d\n%d %d %d\n%d %d %d\n%d %d %d\n%d %d %d\n%d %d %d\n",
 			freq_uv_table[0][0], freq_uv_table[0][1], freq_uv_table[0][2],
 			freq_uv_table[1][0], freq_uv_table[1][1], freq_uv_table[1][2],
 			freq_uv_table[2][0], freq_uv_table[2][1], freq_uv_table[2][2],
@@ -696,7 +698,7 @@ static ssize_t show_frequency_voltage_table(struct cpufreq_policy *policy,
 			freq_uv_table[6][0], freq_uv_table[6][1], freq_uv_table[6][2],
 			freq_uv_table[7][0], freq_uv_table[7][1], freq_uv_table[7][2],
 			freq_uv_table[8][0], freq_uv_table[8][1], freq_uv_table[8][2],
-			freq_uv_table[9][0], freq_uv_table[9][1], freq_uv_table[9][2]
+			freq_uv_table[9][0], freq_uv_table[9][1], freq_uv_table[9][2],
 			freq_uv_table[10][0], freq_uv_table[10][1], freq_uv_table[10][2]);
 
 }
@@ -730,7 +732,7 @@ static ssize_t store_states_enabled_table(struct cpufreq_policy *policy, const c
 
 	ret = sscanf(buf, "%d %d %d %d %d %d %d %d %d %d %d",
 			&enabled_freqs[0], &enabled_freqs[1], &enabled_freqs[2], &enabled_freqs[3],
-			&enabled_freqs[4], &enabled_freqs[5], &enabled_freqs[6], &enabled_freqs[7]
+			&enabled_freqs[4], &enabled_freqs[5], &enabled_freqs[6], &enabled_freqs[7],
 			&enabled_freqs[8], &enabled_freqs[9], &enabled_freqs[10]);
 	if(ret != 1) {
 		return -EINVAL;

381f25a2777a302d40d3478e7ae29a09d8e37155
nubecoder <nubecoder@gmail.com>
2012-04-24 03:11:17 -0400

    initramfs_tw: Some changes to init_01_prep (allows for Samsung qmg boot animation support after 1st boot).
    Signed-off-by: nubecoder <nubecoder@gmail.com>

--- initramfs_tw/nubernel/scripts/init_01_prep.sh
+++ initramfs_tw/nubernel/scripts/init_01_prep.sh
@@ -8,6 +8,7 @@
 #
 
 #defines
+FIRST_BOOT_FILE="/data/local/.first_boot"
 RM_LIST_FILE="/data/local/.rm_file_list"
 
 #functions
@@ -28,21 +29,29 @@ REMOVE_FILE()
 #main
 SEND_LOG "Start"
 
-SEND_LOG "Ensuring there is room for busybox and root"
-SEND_LOG "Looking for $RM_LIST_FILE"
-if /sbin/busybox test -f "$RM_LIST_FILE" ; then
-	SEND_LOG "  Using: $RM_LIST_FILE"
+SEND_LOG "Looking for $FIRST_BOOT_FILE"
+if /sbin/busybox test -f "$FIRST_BOOT_FILE" ; then
+	SEND_LOG "  Found: $FIRST_BOOT_FILE"
+	SEND_LOG "    Skipping removal of files listed in: $RM_LIST_FILE"
 else
-	echo "/system/etc/PowerOn.snd" >"$RM_LIST_FILE"
-	echo "/system/etc/PowerOn.wav" >>"$RM_LIST_FILE"
-	echo "/system/media/bootani.qmg" >>"$RM_LIST_FILE"
-	echo "/system/media/bootsamsungloop.qmg" >>"$RM_LIST_FILE"
-	SEND_LOG "  Created: $RM_LIST_FILE"
+	SEND_LOG "Ensuring there is room for busybox and root"
+	SEND_LOG "  Looking for $RM_LIST_FILE"
+	if /sbin/busybox test -f "$RM_LIST_FILE" ; then
+		SEND_LOG "    Using: $RM_LIST_FILE"
+	else
+		echo "/system/etc/PowerOn.snd" >"$RM_LIST_FILE"
+		echo "/system/etc/PowerOn.wav" >>"$RM_LIST_FILE"
+		echo "/system/media/bootani.qmg" >>"$RM_LIST_FILE"
+		echo "/system/media/bootsamsungloop.qmg" >>"$RM_LIST_FILE"
+		SEND_LOG "    Created: $RM_LIST_FILE"
+	fi
+	SEND_LOG "  Removing files listed in: $RM_LIST_FILE"
+	while read LINE ; do
+		REMOVE_FILE "$LINE"
+	done <"$RM_LIST_FILE"
+	echo "booted already" >"$FIRST_BOOT_FILE"
+	SEND_LOG "  Created: $FIRST_BOOT_FILE"
 fi
-SEND_LOG "  Removing files listed in: $RM_LIST_FILE"
-while read LINE ; do
-	REMOVE_FILE "$LINE"
-done <"$RM_LIST_FILE"
 
 SEND_LOG "Sync filesystem"
 /system/xbin/busybox sync

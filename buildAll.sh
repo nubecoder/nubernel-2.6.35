0743f89d6d89f9fc37f99ba4ead6e4221e72df7d
nubecoder <nubecoder@gmail.com>
2012-05-07 20:23:25 -0400

    scripting: Some scripting changes.
    (Add buildAll script, add heimdall resources, create heimdall package with build script, etc.)
    
    Signed-off-by: nubecoder <nubecoder@gmail.com>

---
+++ buildAll.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# buildAll.sh
+# nubecoder 2012 - http://www.nubecoder.com/
+#
+
+#TODO: Add args support to pass args along to build script.
+START_TIME=$(date +%s)
+echo "Begin building all packagable types."
+./ncBuildHelper.sh -cbmpt tw
+./ncBuildHelper.sh -cbmpt mtd
+./ncBuildHelper.sh -cbmpt cm7
+if [ "$1" = "twrp" ] ; then
+	./ncBuildHelper.sh -cbmpt mtd -r twrp
+	./ncBuildHelper.sh -cbmpt cm7 -r twrp
+fi
+
+echo "Finished building all packagable types."
+END_TIME=$(date +%s)
+echo "buildAll took $(($END_TIME - $START_TIME)) seconds."
+

sgtkwol/android_kernel_samsung_victory

yas529: Fix deadlock when disabling immediately after enabling
Google Navigation does this and deadlocks the entire sensor system

Change-Id: I421c947758e0681c0cfb9ea8809ea00397f4800a

Kernel/drivers/input/misc/yas529.c
@@ -157,6 +157,7 @@ struct geomagnetic_data {
 	atomic_t last_data[3];
 	atomic_t last_status;
 	atomic_t enable;
+	atomic_t disabling;
 	atomic_t filter_enable;
 	atomic_t filter_len;
 	atomic_t delay;
@@ -2327,7 +2328,9 @@ geomagnetic_enable_store(struct device *dev,
 		hwdep_driver.set_enable(value);
 		geomagnetic_enable(data);
 	} else {
+		atomic_set(&data->disabling, 1);
 		geomagnetic_disable(data);
+		atomic_set(&data->disabling, 0);
 		hwdep_driver.set_enable(value);
 	}
 
@@ -2789,9 +2792,12 @@ geomagnetic_input_work_func(struct work_struct *work)
 
 			hwdep_driver.ioctl(YAS529_IOC_GET_DRIVER_STATE,
 				(unsigned long) &state);
-			geomagnetic_multi_lock();
+
+			// Don't lock if disabling as a lock is already held outside
+			// and locking again will cause a deadlock.
+			if (!atomic_read(&data->disabling)) geomagnetic_multi_lock();
 			data->driver_state = state;
-			geomagnetic_multi_unlock();
+			if (!atomic_read(&data->disabling)) geomagnetic_multi_unlock();
 
 			/* report event */
 			code |= (rt & YAS529_REPORT_OVERFLOW_OCCURED);
@@ -2900,6 +2906,7 @@ geomagnetic_probe(struct i2c_client *client, const struct i2c_device_id *id)
 		data->distortion[i] = YAS529_DEFAULT_DISTORTION;
 	data->shape = YAS529_DEFAULT_SHAPE;
 	atomic_set(&data->enable, 0);
+	atomic_set(&data->disabling, 0);
 	for (i = 0; i < 3; i++)
 		atomic_set(&data->last_data[i], 0);
 	atomic_set(&data->last_status, 0);

nubecoder/nubernel-2.6.35

Signed-off-by: nubecoder <nubecoder@gmail.com>

Kernel/drivers/input/misc/yas529.c
@@ -158,6 +158,7 @@ struct geomagnetic_data {
 	atomic_t last_data[3];
 	atomic_t last_status;
 	atomic_t enable;
+	atomic_t disabling;
 	atomic_t filter_enable;
 	atomic_t filter_len;
 	atomic_t delay;
@@ -2293,7 +2294,9 @@ geomagnetic_enable_store(struct device *dev,
 		hwdep_driver.set_enable(value);
 		geomagnetic_enable(data);
 	} else {
+		atomic_set(&data->disabling, 1);
 		geomagnetic_disable(data);
+		atomic_set(&data->disabling, 0);
 		hwdep_driver.set_enable(value);
 	}
 
@@ -2740,9 +2743,12 @@ geomagnetic_input_work_func(struct work_struct *work)
 
 			hwdep_driver.ioctl(YAS529_IOC_GET_DRIVER_STATE,
 				(unsigned long) &state);
-			lock();
+
+			// Don't lock if disabling as a lock is already held outside
+			// and locking again will cause a deadlock.
+			if (!atomic_read(&data->disabling)) lock();
 			data->driver_state = state;
-			unlock();
+			if (!atomic_read(&data->disabling)) unlock();
 
 			/* report event */
 			code |= (rt & YAS529_REPORT_OVERFLOW_OCCURED);
@@ -2902,6 +2908,7 @@ geomagnetic_probe(struct i2c_client *client, const struct i2c_device_id *id)
 		data->distortion[i] = YAS529_DEFAULT_DISTORTION;
 	data->shape = YAS529_DEFAULT_SHAPE;
 	atomic_set(&data->enable, 0);
+	atomic_set(&data->disabling, 0);
 	for (i = 0; i < 3; i++)
 		atomic_set(&data->last_data[i], 0);
 	atomic_set(&data->last_status, 0);